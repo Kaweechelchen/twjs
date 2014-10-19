ddfd483901d216e9ae3df882d51558f8d0a9d5ae
Thierry Degeling <thierry.degeling@gmail.com>
2014-10-20 01:43:34 +0200

    added a search function for tweets

--- app/twjson/jsonControllerProvider.php
+++ app/twjson/jsonControllerProvider.php
@@ -7,7 +7,7 @@
 
     class jsonControllerProvider implements ControllerProviderInterface {
 
-        static public function account ( $handle ) {
+        static public function getAccount ( $handle ) {
 
             /*
              *  https://github.com/J7mbo/twitter-api-php
@@ -38,26 +38,83 @@
 
         }
 
-        static public function getAccount( $app, $handle ) {
+        static public function getSearch ( $handle, $sinceId ) {
 
-            return $app->json( self::account( $handle ) );
+            /*
+             *  https://github.com/J7mbo/twitter-api-php
+             *  Set access tokens here - see: https://dev.twitter.com/apps/
+             *  URL for REST request, see: https://dev.twitter.com/docs/api/1.1/
+             */
+
+            $url = 'https://api.twitter.com/1.1/search/tweets.json';
+            $getfield = "?q=$handle%20-RT&count=100&result_type=recent";
+            if ( $sinceId != 0 ) {
+
+                $getfield .= "&since_id=$sinceId";
+
+            }
+            $requestMethod = 'GET';
+
+            $settings = array(
+                'consumer_key'              => getenv('apikey'),
+                'consumer_secret'           => getenv('apisecret'),
+                'oauth_access_token'        => getenv('accesstoken'),
+                'oauth_access_token_secret' => getenv('accesstokensecret')
+            );
+
+            $settings = array(
+                'consumer_key'              => 'rPQ1DtNK2K0v1ZooarswsqRMc',
+                'consumer_secret'           => 'It6qy9Hz6dcZiipmBB2SH1c7SuEHZjBrUarM781e2oeC3tA4MT',
+                'oauth_access_token'        => '260828691-LSkn4brLVTrIYhru6TlLT5kx6EWDKZHqtqDcoMUI',
+                'oauth_access_token_secret' => '0Yhi2UhCA0kgEoh7PXunNu3pFFMcx7VePnnm50UJ0nQhe'
+            );
+
+            $twitter = new TwitterAPIExchange( $settings );
+            $twitterJSON = json_decode(
+                $twitter->setGetfield( $getfield )
+                        ->buildOauth( $url, $requestMethod )
+                        ->performRequest(),
+                true
+            );
+
+            return ( $twitterJSON );
 
         }
 
-        static public function getCleanAccount( $app, $handle) {
+        static public function clean( $twitterJSON ) {
 
-            $twitterJSON = self::account( $handle );
+            if ( isset( $twitterJSON[ 'statuses' ] )) {
+
+                $twitterJSON = $twitterJSON[ 'statuses' ];
+
+            }
 
             foreach ( $twitterJSON as $twitterTweet) {
 
                 unset( $tweet );
 
+                $tweet[ 'id'  ] = $twitterTweet[ 'id' ];
                 $tweet[ 'uts' ] = strtotime( $twitterTweet[ 'created_at' ] );
-                $tweet[ 'txt' ]   = $twitterTweet[ 'text' ];
+                $tweet[ 'txt' ] = $twitterTweet[ 'text' ];
+                $tweet[ 'user' ][ 'handle' ]    = $twitterTweet[ 'user' ][ 'screen_name' ];
+                $tweet[ 'user' ][ 'name' ]    = $twitterTweet[ 'user' ][ 'name' ];
+                $tweet[ 'user' ][ 'profilePic' ] = str_replace(
+                    '_normal',
+                    '',
+                    $twitterTweet[ 'user' ][ 'profile_image_url_https' ]
+                );
 
                 if( array_key_exists( 'extended_entities', $twitterTweet ) ) {
 
                     foreach ( $twitterTweet[ 'extended_entities' ][ 'media' ] as $image ) {
+
+                        $tweet[ 'img' ][] = $image[ 'media_url_https' ];
+
+                    }
+
+                } else if ( array_key_exists( 'media', $twitterTweet[ 'entities' ] ) ){
+
+                    foreach ( $twitterTweet[ 'entities' ][ 'media' ] as $image ) {
 
                         $tweet[ 'img' ][] = $image[ 'media_url_https' ];
 
@@ -71,7 +128,7 @@
 
             array_slice($tweets, 1, 1, true);
 
-            return $app->json( $tweets );
+            return $tweets;
 
         }
 
@@ -85,15 +142,39 @@
 
             });
 
+            $ctr->get( '/search/{twitterHandle}/{sinceId}', function( Application $app, $twitterHandle, $sinceId ) {
+
+                $tweets = self::getSearch( $twitterHandle, $sinceId );
+
+                return $app->json( $tweets );
+
+            })->value('sinceId', 0);
+
+            $ctr->get( '/clean/search/{twitterHandle}/{sinceId}', function( Application $app, $twitterHandle, $sinceId ) {
+
+                $tweets = self::clean(
+                    self::getSearch( $twitterHandle, $sinceId )
+                );
+
+                return $app->json( $tweets );
+
+            })->value('sinceId', 0);
+
             $ctr->get( '/clean/{twitterHandle}', function( Application $app, $twitterHandle ) {
 
-                return self::getCleanAccount( $app, $twitterHandle );
+                $tweets = self::clean(
+                    self::getAccount( $twitterHandle )
+                );
+
+                return $app->json( $tweets );
 
             });
 
             $ctr->get( '/{twitterHandle}', function( Application $app, $twitterHandle ) {
 
-                return self::getAccount( $app, $twitterHandle );
+                $tweets = self::getAccount( $twitterHandle );
+
+                return $app->json( $tweets );
 
             });
 
